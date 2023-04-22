f98522e04a262f63323ea6a2a802ed7716871196
Blaine Billingsley <blainebillingsley@gmail.com>
2023-04-21 22:39:36 -0700

    Remove animations oopsie

--- js/app2.js
+++ js/app2.js
@@ -1,58 +1,58 @@
-const header = document.getElementById('nav');
-window.addEventListener('load', function() {
-  header.classList.remove('preload');
-});
+// const header = document.getElementById('nav');
+// window.addEventListener('load', function() {
+//   header.classList.remove('preload');
+// });
 
-gsap.registerPlugin(ScrollTrigger); /* register the ScrollTrigger plugin */
-const animatables = gsap.utils.toArray(".fadeIn");
-animatables.forEach(function(element) {
-  gsap.set(element, {
-    y: 60,
-    opacity: 0,
-    ease: 'power2.in',
-    overwrite: 'auto',
-  });
-  ScrollTrigger.create({
-    trigger: element,
-    start: 'top 90%',
-    end: 'top top',
-    markers: false,
-    onEnter: function() {
-      gsap.to(element, {
-        y: 0,
-        opacity: 1,
-        duration: 0.4, // set duration for the animation
-      });
-    },
-  });
-});
+// gsap.registerPlugin(ScrollTrigger); /* register the ScrollTrigger plugin */
+// const animatables = gsap.utils.toArray(".fadeIn");
+// animatables.forEach(function(element) {
+//   gsap.set(element, {
+//     y: 60,
+//     opacity: 0,
+//     ease: 'power2.in',
+//     overwrite: 'auto',
+//   });
+//   ScrollTrigger.create({
+//     trigger: element,
+//     start: 'top 90%',
+//     end: 'top top',
+//     markers: false,
+//     onEnter: function() {
+//       gsap.to(element, {
+//         y: 0,
+//         opacity: 1,
+//         duration: 0.4, // set duration for the animation
+//       });
+//     },
+//   });
+// });
 
-// Get all the sections on the page
-const sections = document.querySelectorAll('section');
+// // Get all the sections on the page
+// const sections = document.querySelectorAll('section');
 
-// Check if a section is in the viewport
-function isInViewport(section) {
-  const rect = section.getBoundingClientRect();
-  return (
-    rect.top <= (window.innerHeight || document.documentElement.clientHeight) / 2 &&
-    rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) / 2
-  );
-}
+// // Check if a section is in the viewport
+// function isInViewport(section) {
+//   const rect = section.getBoundingClientRect();
+//   return (
+//     rect.top <= (window.innerHeight || document.documentElement.clientHeight) / 2 &&
+//     rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) / 2
+//   );
+// }
 
-// Add the "animate" class to any section that is in the viewport
-function animateSections() {
-  sections.forEach((section) => {
-    if (isInViewport(section)) {
-      section.classList.add('animate');
-    } else {
-      section.classList.remove('animate');
-    }
-  });
-}
+// // Add the "animate" class to any section that is in the viewport
+// function animateSections() {
+//   sections.forEach((section) => {
+//     if (isInViewport(section)) {
+//       section.classList.add('animate');
+//     } else {
+//       section.classList.remove('animate');
+//     }
+//   });
+// }
 
-// Call the animateSections function whenever the window is scrolled or resized
-window.addEventListener('scroll', animateSections);
-window.addEventListener('resize', animateSections);
+// // Call the animateSections function whenever the window is scrolled or resized
+// window.addEventListener('scroll', animateSections);
+// window.addEventListener('resize', animateSections);
 
 // var element = document.querySelector(".fadeIn"); /* get the element */
 
@@ -294,41 +294,6 @@ form.addEventListener("submit", function(event) {
   }
 });
 
-
-// const $bigBall = document.querySelector('.cursor__ball--big');
-// const $smallBall = document.querySelector('.cursor__ball--small');
-// const $hoverables = document.querySelectorAll('.hoverable');
-
-// // Listeners
-// document.body.addEventListener('mousemove', onMouseMove);
-// for (let i = 0; i < $hoverables.length; i++) {if (window.CP.shouldStopExecution(0)) break;
-//   $hoverables[i].addEventListener('mouseenter', onMouseHover);
-//   $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
-// }
-
-// // Move the cursor
-// window.CP.exitedLoop(0);function onMouseMove(e) {
-//   TweenMax.to($bigBall, .4, {
-//     x: e.pageX - 15,
-//     y: e.pageY - 15 });
-
-//   TweenMax.to($smallBall, .1, {
-//     x: e.pageX - 5,
-//     y: e.pageY - 7 });
-
-// }
-
-// // Hover an element
-// function onMouseHover() {
-//   TweenMax.to($bigBall, .3, {
-//     scale: 4 });
-
-// }
-// function onMouseHoverOut() {
-//   TweenMax.to($bigBall, .3, {
-//     scale: 1 });
-
-// }
 
 
 // Check if a section is in the viewport
